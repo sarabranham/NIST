c11064478cf364f92d11c76588b42dc74e3fe5aa
sarabranham <sara.m.branham@gmail.com>
2017-06-22 11:51:30 -0600

    Changed init to be compatible w/ fitting - errors if given not given arg (if doesn't work as expected)
    Goal: add in uncertainties/make graphs less boring

--- TwoPortModels.py
+++ TwoPortModels.py
@@ -10,13 +10,17 @@
 try:
     import math
 except:
-    print("The module math either was not found"
-          "Please put it on the python path")
+    print("The module math either was not found or has an error")
     raise
 try:
     import numpy as np
 except:
     print("The module numpy either was not found or has an error")
+    raise
+try:
+    import sympy
+except:
+    print("The module sympy either wa snot found or has an error")
     raise
 
 # Know: frequency and device (short, open, load)
@@ -25,13 +29,30 @@ except:
 
 
 class SimpleTwoPort(object):
-    z0 = 50.
-    zg = 0.
 
-    def __init__(self, freq, r):
-        # set default r if no r
-        self.f = freq
-        self.zl = r
+    def __init__(self, **kwargs):
+        defaults = {"frequency": None, "resistance": None}
+        self.options = {}
+        for key, value in defaults.iteritems():
+            self.options[key] = value
+        for key, value in kwargs.iteritems():
+            self.options[key] = value
+        if self.options["frequency"]:
+            self.f = self.options["frequency"]
+        else:
+            self.f = [3e8, 4e9, 5e10]
+        if self.options["resistance"]:
+            self.zl = self.options["resistance"]
+        else:
+            self.zl = 5.
+
+        # for some reason it is angry if I address vars not in dict
+#        if self.options["char_imp"]:
+#            self.z0 = self.options["char_imp"]
+#        else:
+        self.z0 = 50.
+#        if self.options["length"]:
+#            self.len = self.options["length"]
 
     def get_freq(self):
         return self.f
@@ -63,50 +84,62 @@ class SimpleTwoPort(object):
 
 class OpenTwoPort(SimpleTwoPort):
 
-    def __init__(self, freq, r, capacitance):
-        SimpleTwoPort.__init__(self, freq, r)
-        self.c = capacitance
+    def __init__(self, **kwargs):
+        defaults = {"frequency": None, "capacitance": None}
+        self.options = {}
+        for key, value in defaults.iteritems():
+            self.options[key] = value
+        for key, value in kwargs.iteritems():
+            self.options[key] = value
+        # test if any vars in superclass
+        SimpleTwoPort.__init__(self, **self.options)
+        if self.options["capacitance"]:
+            self.c = self.options["capacitance"]
+        else:
+            self.c = .000047
         self.z = [1 / (2 * math.pi * self.f[i] * self.c) for i in range(len(self.f))]
-
-    def data(self):
-        a = [[self.f[i]] for i in range(len(self.f))]
-        for j in range(len(a)):
-            for p in self.calc_s(self.z[j]):
-                a[j].append(float("{0:.8f}".format(p)))
-        return a
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 
 class ShortTwoPort(SimpleTwoPort):
 
-    def __init__(self, freq, r, inductance):
-        SimpleTwoPort.__init__(self, freq, r)
-        self.i = inductance
+    def __init__(self, **kwargs):
+        defaults = {"frequency": None, "inductance": None}
+        self.options = {}
+        for key, value in defaults.iteritems():
+            self.options[key] = value
+        for key, value in kwargs.iteritems():
+            self.options[key] = value
+        # test if any vars in superclass
+        SimpleTwoPort.__init__(self, **self.options)
+        if self.options["inductance"]:
+            self.i = self.options["inductance"]
+        else:
+            self.i = .000910
+#        if self.options["impedance"]:
+#            self.z = self.options["impedance"]
+#        else:
         self.z = [2 * math.pi * self.f[j] * self.i for j in range(len(self.f))]
 
-    def data(self):
-        a = [[self.f[i]] for i in range(len(self.f))]
-        for j in range(len(a)):
-            for p in self.calc_s(self.z[j]):
-                a[j].append(float("{0:.8f}".format(p)))
-        return a
-
 # -----------------------------------------------------------------------------------------------------------------------
-f = [4e11, 35e11, 3e13]
+f = [3e8, 4e9, 5e10]
 
 
 def test_two_port_model():
     # Expect: vals for s11, s22; others = 0
     # Get: s11 = 1, s12 = 0, s21 = 0, s22 = 1
-    x = ShortTwoPort(f, 50, .000910)
-    y = OpenTwoPort(f, 50, .000047)
+    x = ShortTwoPort(frequency=f, resistance=50, inductance=.000910)
+    y = OpenTwoPort(frequency=f, resistance=50, capacitance=.000047)
 
     # Expect: all small values???
     # Get: s11 = 1, s12 = 0.009, s21 = 0.009, s22 = 1
-    z = SimpleTwoPort(f, 0.001)
+    z = SimpleTwoPort(frequency=f, resistance=0.1)
 
     print x.data()
     print y.data()
     print z.data()
+
+
+test_two_port_model()
 
