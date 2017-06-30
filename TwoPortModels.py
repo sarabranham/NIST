d2f3c2ad48eae45eee82600f2f9651d79eabc8c0
sarabranham <sara.m.branham@gmail.com>
2017-06-30 14:53:20 -0600

    Fixing Complex Plots

--- TwoPortModels.py
+++ TwoPortModels.py
@@ -24,7 +24,7 @@ except:
     raise
 try:
     import sympy as sympy
-    from sympy.abc import f, F, c, C, zeta, l, S, Z, R
+    from sympy.abc import f, F, c, C, zeta, l, S, Z
 except:
     print("The module sympy either wa snot found or has an error")
     raise
@@ -38,11 +38,13 @@ try:
 except:
     print("The module cmath either was not found or has an error")
     raise
+try:
+    import re
+except:
+    print("The module re either was not found or has an error")
+    raise
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Know: frequency and device (short, open, load)
-# other parameters change by device (length and waveguide)
-# when call w/ list of freqs
 
 
 class TwoPortModel(object):
@@ -72,7 +74,7 @@ class TwoPortModel(object):
         if self.options["length"]:
             self.len = self.options["length"]
         self.eqn = sympy.Matrix(np.array([[(Z - zeta)/(Z + zeta), sympy.sqrt(Z/zeta)*(1 - abs(S))],
-                                          [(zeta - Z)/(Z + zeta), sympy.sqrt(Z/zeta)*(1 - abs(S))]]))
+                                          [sympy.sqrt(Z/zeta)*(1 - abs(S)), (zeta - Z)/(Z + zeta)]]))
 
         # define eqns? - not sure if it should be an instance var?
 
@@ -161,6 +163,10 @@ class OpenModel(TwoPortModel):
             if self.options["impedance"]:
                 self.z = self.options["impedance"]
             else:
+                c0, c1, c2 = sympy.symbols('c0 c1 c2')
+                z_c = 1 / (2 * sympy.pi * sympy.I * (c0 + c1 * f + c2 * f ** 2) * f)
+                for i in range(len(self.eqn)):
+                    self.eqn[i] = self.eqn[i].subs(zeta, z_c)
                 try:
                     self.c0 = self.options['c0']
                     self.c1 = self.options['c1']
@@ -176,16 +182,6 @@ class OpenModel(TwoPortModel):
 
                     print "Default parameter input c0=c1=c2=" + str(self.c0)
 
-    def get_eqns(self):
-        if not self.complex:
-            TwoPortModel.get_eqns(self)
-        else:
-            c0, c1, c2 = sympy.symbols('c0 c1 c2')
-            z_c = 1/(2*sympy.pi*sympy.I*(c0 + c1*f + c2*f**2)*f)
-            for i in range(len(self.eqn)):
-                self.eqn[i] = self.eqn[i].subs(zeta, z_c).simplify()
-            TwoPortModel.get_eqns(self)
-
     def set_complex_coefs(self, c0, c1, c2):
         self.c0 = c0
         self.c1 = c1
@@ -194,10 +190,10 @@ class OpenModel(TwoPortModel):
                   for i in range(len(self.f))]
 
     def complex_calc_s(self, z):
-        s11 = complex(self.z0, z) / complex(self.z0, z)
-        s21 = cmath.sqrt(self.z0/complex(0, z)) * (1 - s11)
+        s11 = complex(self.z0, -z) / complex(self.z0, z)
+        s21 = cmath.sqrt(self.z0/complex(0, z)) * (1 - np.absolute(s11))
         s22 = complex(-self.z0, z) / complex(self.z0, z)
-        s12 = cmath.sqrt(self.z0/complex(0, z)) * (1 - 22)
+        s12 = cmath.sqrt(self.z0/complex(0, z)) * (1 - np.absolute(s22))
         return s11, s12, s21, s22
 
     def data(self):
@@ -255,6 +251,10 @@ class ShortModel(TwoPortModel):
             if self.options["impedance"]:
                 self.z = self.options["impedance"]
             else:
+                l0, l1, l2 = sympy.symbols('l0 l1 l2')
+                z_l = 2 * sympy.pi * sympy.I * (l0 + l1 * f + l2 * f ** 2)
+                for i in range(len(self.eqn)):
+                    self.eqn[i] = self.eqn[i].subs(zeta, z_l).simplify()
                 try:
                     self.l0 = self.options['l0']
                     self.l1 = self.options['l1']
@@ -269,16 +269,6 @@ class ShortModel(TwoPortModel):
                               i in range(len(self.f))]
                     print "Default parameter input l0=l1=l2=" + str(self.l0)
 
-    def get_eqns(self):
-        if not self.complex:
-            TwoPortModel.get_eqns(self)
-        else:
-            l0, l1, l2 = sympy.symbols('l0 l1 l2')
-            z_l = 1/(2*sympy.pi*sympy.I*(l0 + l1*f + l2*f**2)*f)
-            for i in range(len(self.eqn)):
-                self.eqn[i] = self.eqn[i].subs(zeta, z_l).simplify()
-            TwoPortModel.get_eqns(self)
-
     def set_complex_coefs(self, l0, l1, l2):
         self.l0 = l0
         self.l1 = l1
@@ -287,10 +277,10 @@ class ShortModel(TwoPortModel):
                   i in range(len(self.f))]
 
     def complex_calc_s(self, z):
-        s11 = complex(self.z0, z) / complex(self.z0, z)
-        s21 = cmath.sqrt(self.z0/complex(0, z)) * (1 - s11)
+        s11 = complex(self.z0, -z) / complex(self.z0, z)
+        s21 = cmath.sqrt(self.z0/complex(0, z)) * (1 - np.absolute(s11))
         s22 = complex(-self.z0, z) / complex(self.z0, z)
-        s12 = cmath.sqrt(self.z0/complex(0, z)) * (1 - 22)
+        s12 = cmath.sqrt(self.z0/complex(0, z)) * (1 - np.absolute(s22))
         return s11, s12, s21, s22
 
     def data(self):
@@ -311,16 +301,69 @@ class ShortModel(TwoPortModel):
 
 
 def test_two_port_model():
-    # Expect: vals for s11, s22; others = 0
-    # Get: s11 = -1/1, s12 = 0/0.009, s21 = 0/0.009, s22 = 1/-1
-    short_complex = ShortModel(complex=False)
-    open_complex = OpenModel(complex=False)
-    # Expect: all small values???
-    # Get: s11 = 1, s12 = 0.09, s21 = 0.09, s22 = 1
-    # z = TwoPortModel(frequency=freq, resistance=18)
-    print short_complex.data()
+    # Short/Open: vals for s11, s22; others = 0, Get: s11 = -1/1, s12 = 0/0.009, s21 = 0/0.009, s22 = 1/-1
+    # Load: all small values, get: s11 = 0.84, s12 = 0.57, s21 = 0.57, s22 = 0.84
+    open_complex = OpenModel(complex=True)
+    short_complex = ShortModel(complex=True)
     print open_complex.data()
-    # short_complex.get_eqns()
+    print short_complex.data()
+
+
+def plot_params(model_type, **kwargs):
+    """ Assumes instantiated model will be input, plots the model according to parameters and keyword 'format'
+        RI = real imaginary vs. freq, MP = magnitude phase vs. freq, default = s parameter vs. freq """
+    defaults = {"format": None}
+    plot_options = {}
+    for key, value in defaults.iteritems():
+        plot_options[key] = value
+    for key, value in kwargs.iteritems():
+        plot_options[key] = value
+
+    # Complex Plots
+    if model_type.complex:
+        if type(model_type) == OpenModel:
+            s11 = FunctionalModel(parameters=['Z', 'c0', 'c1', 'c2'], variables='f', equation=model_type.eqn[0])
+            s12 = FunctionalModel(parameters=['Z', 'c0', 'c1', 'c2', 's'], variables='f', equation=model_type.eqn[1])
+            s21 = FunctionalModel(parameters=['Z', 'c0', 'c1', 'c2', 's'], variables='f', equation=model_type.eqn[2])
+            s22 = FunctionalModel(parameters=['Z', 'c0', 'c1', 'c2'], variables='f', equation=model_type.eqn[3])
+            return [s11.equation, s12.equation, s21.equation, s22.equation]
+        else:
+            s11 = FunctionalModel(parameters=['Z', 'l0', 'l1', 'l2'], variables='f', equation=model_type.eqn[0])
+            s12 = FunctionalModel(parameters=['Z', 'l0', 'l1', 'l2', 's'], variables='f', equation=model_type.eqn[1])
+            s21 = FunctionalModel(parameters=['Z', 'l0', 'l1', 'l2', 's'], variables='f', equation=model_type.eqn[2])
+            s22 = FunctionalModel(parameters=['Z', 'l0', 'l1', 'l2'], variables='f', equation=model_type.eqn[3])
+            return [s11.equation, s12.equation, s21.equation, s22.equation]
+
+    # Real Instantiation
+    else:
+        if type(model_type) == OpenModel:
+            s11 = 4
+        elif type(model_type) == ShortModel:
+            s11 = 4
+        elif type(model_type) == ReciprocalModel:
+            s11 = 4
+        else:
+            s11 = 4
+
+    # Real/Imaginary Plot
+    if re.search('r', plot_options['format'], re.IGNORECASE):
+        # Do real/imaginary stuff
+        return
+
+    # Magnitude/Phase Plot
+    elif re.search('m', plot_options['format'], re.IGNORECASE):
+        # Do magnitude/phase stuff
+        return
+
+    # Smith Plot
+    elif re.search('smith', plot_options['format'], re.IGNORECASE):
+        # Do Smith Plot Stuff
+        return
+
+    # Parameter Frequency Plot
+    else:
+        # Do Parameter Frequency Stuff
+        return
 
 
 def get_s_param_eqns(eqn):
@@ -328,70 +371,56 @@ def get_s_param_eqns(eqn):
 
 
 def graph_s(circuit_type):
-    freq = np.linspace(3e8, 5e10, 500)
+    f = np.linspace(3e8, 5e10, 500)
     if circuit_type == 'Open' or circuit_type == 'open':
-        z = OpenModel(frequency=freq, resistance=50, capacitance=.000047)
-        # p_refl = sympy.lambdify((f, c, zeta), expr.subs((F, C, R), (f, c, zeta)))
-        p_trans = sympy.lambdify((f, c, zeta, S), (2*math.pi*f*c*zeta)**(1/2)*(1-S), 'math')
-
+        z = OpenModel(frequency=f, resistance=50, capacitance=.000047)
     elif circuit_type == 'Short' or circuit_type == 'short':
-        z = ShortModel(frequency=freq, resistance=50, inductance=.000910)
-        p_refl = sympy.lambdify((f, l, zeta), (zeta - 2*math.pi*f*l) / (zeta + 2*math.pi*f*l), 'math')
-        p_trans = sympy.lambdify((f, l, zeta, S), (math.sqrt(zeta / (2*math.pi*f*l)) * (1 - S)), 'math')
-
+        z = ShortModel(frequency=f, resistance=50, inductance=.000910)
     else:
-        z = TwoPortModel(frequency=freq, resistance=0.1)
-        p_refl = sympy.lambdify((zeta, Z), (zeta - Z) / (zeta + Z))
-        p_trans = sympy.lambdify((zeta, Z, S), (zeta / Z) ** (1 / 2) * (1 - S))
-
+        z = TwoPortModel(frequency=f, resistance=0.1)
     count = 0
     s_data = [[], [], [], []]
     for list1 in z.data():
         for i in range(len(list1)):
             if i > 0:
                 s_data[i - 1].append(list1[i])
-
     refl_line = FunctionalModel(parameters=["z0"], variables="z", equation="(z0-z)/(z0+z)")
     tran_line = FunctionalModel(parameters=["z0", "s"], variables="z", equation="(z0/z)**(1/2)*(1-s)")
     eqns = []
-    params = []
     for list1 in s_data:
         if count == 0 or count == 3:
-            refl_line.fit_data(freq, list1)
+            refl_line.fit_data(f, list1)
             eqns.append(refl_line.equation)
-            plt.plot(freq, list1, label="s11" if count == 0 else "s22")
-            if circuit_type == 'Open' or circuit_type == 'open':
-                params.append(p_refl(np.array(freq), z.c, z.z0))
-            elif circuit_type == 'Short' or circuit_type == 'short':
-                params.append(p_refl(np.array(freq), z.i, z.z0))
-            else:
-                params.append(p_refl(np.array(freq), np.array(list1)))
+            plt.plot(f, list1, label="s11" if count == 0 else "s22")
             count += 1
         else:
-            tran_line.fit_data(freq, list1)
+            tran_line.fit_data(f, list1)
             eqns.append(tran_line.equation)
-            plt.plot(freq, list1, label="s12" if count == 1 else "s21")
-            if circuit_type == 'Open' or circuit_type == 'open':
-                params.append(p_trans(np.array(freq), z.c, z.z0, np.array(s_data)))
-            elif circuit_type == 'Short' or circuit_type == 'short':
-                params.append(p_trans(np.array(freq), z.c, z.z0))
-            else:
-                params.append(p_trans(np.array(freq), z.c, z.z0))
+            plt.plot(f, list1, label="s12" if count == 1 else "s21")
             count += 1
 
-    # TODO fix this title if possible (works in JN)
+    # TODO fix this title because it's not happening
     sympy.init_printing()
     sympy.pprint(sympy.Matrix(get_s_param_eqns(eqns)), use_unicode=False)
     print ' '
-    print params
+    sympy.pprint(sympy.Matrix(get_s_param_eqns(eqns)), use_unicode=False)
     plt.title(circuit_type + ' S Parameters')
     plt.legend()
     plt.show()
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-test_two_port_model()
-# graph_s('open')
+# test_two_port_model()
+z = OpenModel(complex=True)
+print plot_params(z, format='RI')
+
+print "Test"
+
+y = ShortModel(complex=True)
+print plot_params(y, format='RI')
+
+
+
 
 
 
